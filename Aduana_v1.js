59b2ed877631cc47390ace6288409e1d5129c209
kevin-algebra <80739831+kevin-algebra@users.noreply.github.com>
2022-03-05 14:03:04 -0600

    Update Aduana_v1.js

--- Aduana_v1.js
+++ Aduana_v1.js
@@ -3,7 +3,8 @@ console.log("Buenas tardes , Aduana de USA");
 
 let name="Longino";
 
-let NIP=1000;
+let NIP=12345678;
+
 
 let oportunidad = "No";
 
@@ -12,13 +13,13 @@ let ala=(`${name} , ¿Es usted ilegal?`);
 console.log(ala)
 
 
-let hour= "baba";
+let hour= "ala";
 
 
 
 if (hour == "No") {
     console.log(`${name} , ¿Podria proporcionarme el NIP de su carnet de identidad?`);
-    if (NIP.lenght == 9) {
+    if (NIP<99999999 && NIP>0) {
         console.log(`Todo en orden ${name} , de vuelta a casa!`);
     } else {
         console.log(`Lo siento señor ${name} , tendra que acompañarme a la comisaria...`)
@@ -29,15 +30,15 @@ if (hour == "No") {
     console.log(`${name} , responda mi pregunta por favor o sera llevado a la comisaria de
     Houston Texas , le dare una segunda oportunidad de hacer las cosas por el buen camino.`);
     
-    if (hour == "No") {
+    if (oportunidad == "No") {
     console.log(`${name} , ¿Podria proporcionarme el NIP de su carnet de identidad?`);
-    if (name.lenght == 8) {
+    if (NIP<99999999 && NIP>0) {
         console.log(`Todo en orden ${name} , de vuelta a casa!`);
     } else {
         console.log(`Lo siento señor ${name} , tendra que acompañarme a la comisaria...`)
     }
     
-    } else if (hour == "Si") {
+    } else if (oportunidad == "Si") {
     console.log(`${name} , Un oficial de migracion lo acompañara al proximo camion de deportacion`); 
     } else {
         console.log(`${name} queda arrestado.`)
@@ -46,6 +47,11 @@ if (hour == "No") {
 }
     
     
+
+
+
+
+
 
 
 
