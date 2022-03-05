d265f99b6e62ca21acb15d99a238834cedcc0d29
kevin-algebra <80739831+kevin-algebra@users.noreply.github.com>
2022-03-05 13:23:35 -0600

    Create Aduana_v0.js

---
+++ Aduana_v0.js
@@ -0,0 +1,14 @@
+console.log("Buenas tardes , Aduana de USA");
+
+let name= window.prompt("¿Podria proporcionarme su nombre?")
+
+let hour=window.prompt(`${name} , ¿Es usted ilegal?`);
+
+if (hour=si) {
+    console.log(`${name} , ¿Podria proporcionarme su carnet de identidad?`);
+} else if (no) {
+    console.log(`${name} , Un oficial de migracion lo acompañara al proximo camion de deportacion`);
+} else {
+     console.log(`${name} , responda mi pregunta por favor o sera llevado a la comisaria de
+    Houston Texas`);
+}
